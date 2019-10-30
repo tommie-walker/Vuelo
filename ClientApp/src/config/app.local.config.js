69cce77da263a12c7b2e40a2a80aa3357b8113f7
tommie.walker <tommie.walker@ruralsourcing.com>
2019-10-30 15:03:26 -0500

    Added the path to add a new Helicopter

--- ClientApp/src/config/app.local.config.js
+++ ClientApp/src/config/app.local.config.js
@@ -1,6 +1,7 @@
 const config = {
   userServiceUrl: "https://localhost:5001/api/User/",
-  helicopterServiceUrl: "http://localhost:8080/api/helicopter/"
+  helicopterServiceUrl: "http://localhost:8080/api/helicopter/",
+  addHelicopterServiceUrl: "http://localhost:8080/api/createHelicopter/"
 };
 
 export default config;
